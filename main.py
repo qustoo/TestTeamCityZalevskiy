86268468371b9d88c29c6d1415914a71c08b8292
qustoo <zalevskiy@sfedu.ru>
2022-12-18 20:45:49 +0300

    broke project #6

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import pytest
 def Factorial_error(n):
     isGood = str(n).isnumeric()
     if not isGood: # if incorrect input
@@ -9,8 +9,11 @@ def Factorial_error(n):
             result*=i
         return result
 
+def test_error():
+    n= Factorial_error(5)
+    assert n == 20
+
 if __name__ == '__main__':
     print('Waiting for input n...')
-    # n = input()
-    n = "haha"
-    print(f'factorial({n}) = {Factorial(n)}')
+    n = input()
+    print(f'for n ={n}, factorial = {Factorial_error(n)}')
