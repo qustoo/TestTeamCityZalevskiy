224993a21d9439deae937c2a1dda345d6172c909
qustoo <zalevskiy@sfedu.ru>
2022-12-18 18:40:53 +0300

    added tests to factorial

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 def Factorial(n):
+    try:
+        n = int(n)
+    except ValueError:
+        print("Incorrect input!")
+        return
     result = 1
     for i in range(2,n+1):
         result*=i
@@ -6,5 +11,5 @@ def Factorial(n):
 
 if __name__ == '__main__':
     print('Waiting for input n...')
-    n = int(input())
+    n = input()
     print(f'factorial({n}) = {Factorial(n)}')
---
+++ test.py
@@ -0,0 +1,21 @@
+from main import Factorial
+import unittest
+
+class TestMyFactoral(unittest.TestCase):
+    def test_negative_number(self):
+        res = Factorial(-1)
+        self.assertEqual(res,None)
+
+    def test_positive_small_number(self):
+        res = Factorial(5)
+        self.assertEqual(res,120)
+
+    def test_positive_big_number(self):
+        res = Factorial(20)
+        self.assertEqual(res,2432902008176640000)
+
+    def test_incorrect_input(self):
+        res = Factorial("hahaha")
+        self.assertEqual(res,ValueError)
+
+
