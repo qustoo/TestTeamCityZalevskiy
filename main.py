d048a34298c8a64d12f11e4ab8aa33aa1f65d18c
qustoo <zalevskiy@sfedu.ru>
2022-12-22 22:47:39 +0300

    update: check error v2

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ def Factorial(n):
             result*=i
         return result
 
+def test_error():
+    n = Factorial(3)
+    assert n == 25
 
 if __name__ == '__main__':
     print('Waiting for input n...')
-    n = 5
+    n = 0.5
+    # test_error()
+    unittest.main()
     print(f'for n ={n}, factorial = {Factorial(n)}')
