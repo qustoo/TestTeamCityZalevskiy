aaee4463644dfc7623e99970e02ea8d086b8280e
qustoo <zalevskiy@sfedu.ru>
2022-12-22 22:49:17 +0300

    name is not defined

--- main.py
+++ main.py
@@ -9,13 +9,8 @@ def Factorial(n):
             result*=i
         return result
 
-def test_error():
-    n = Factorial(3)
-    assert n == 25
 
 if __name__ == '__main__':
     print('Waiting for input n...')
     n = 0.5
-    # test_error()
-    unittest.main()
-    print(f'for n ={n}, factorial = {Factorial(n)}')
+    print(f'for n ={n}, factorial = {Factorial_error(n)}')
